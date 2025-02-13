0bd0050b7c7502c475d2251e437980d072d4e6c6
evaristocuesta <evaristocg@gmail.com>
2025-02-13 20:55:31 +0100

    Add cookies and privacy

---
+++ src/ViveCortelazor/Services/MarkdownService.cs
@@ -0,0 +1,44 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using ViveCortelazor.Pipelines;
+
+namespace ViveCortelazor.Services;
+
+public class MarkdownService : IMarkdownService
+{
+    private readonly MarkdownPipeline _pipeline;
+
+    public MarkdownService()
+    {
+        var builder = new MarkdownPipelineBuilder()
+            .UseAdvancedExtensions()
+            .UseBootstrap();
+
+        _pipeline = builder.Build();
+
+        var writer = new StringWriter();
+        var renderer = new HtmlRenderer(writer);
+        _pipeline.Setup(renderer);
+
+        //renderer.ObjectRenderers.RemoveAll(r => r is ListRenderer);
+        //renderer.ObjectRenderers.Add(new CustomListRenderer());
+    }
+
+    public string ToHtml(string markdown)
+    {
+        var writer = new StringWriter();
+        var renderer = new HtmlRenderer(writer);
+        _pipeline.Setup(renderer);
+
+        //if (!renderer.ObjectRenderers.Any(r => r is CustomListRenderer))
+        //{
+        //    renderer.ObjectRenderers.RemoveAll(r => r is ListRenderer);
+        //    renderer.ObjectRenderers.Add(new CustomListRenderer());
+        //}
+
+        var document = Markdown.Parse(markdown, _pipeline);
+        renderer.Render(document);
+        return writer.ToString();
+    }
+}
---
+++ src/ViveCortelazor/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using ViveCortelazor.Services;
+
+namespace ViveCortelazor.TagHelpers;
+
+public class MarkdownTagHelper : TagHelper
+{
+    private readonly IMarkdownService _markdownService;
+
+    public MarkdownTagHelper(IMarkdownService markdownService)
+    {
+        _markdownService = markdownService;
+    }
+
+    public string Content { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = null;
+        output.Content.SetHtmlContent(_markdownService.ToHtml(Content));
+    }
+}
