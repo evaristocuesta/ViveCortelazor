2533808bb876480c91bd21caa5128e3e4b1c5bc1
evaristocuesta <evaristocg@gmail.com>
2025-02-14 15:55:39 +0100

    Add tests #55

--- tests/ViveCortelazor.Tests/ViveCortelazorTests.cs
+++ tests/ViveCortelazor.Tests/ViveCortelazorTests.cs
@@ -10,6 +10,10 @@ public class ViveCortelazorTests : PageTest
     [TestCase("", "Vive Cortelazor - Sierra de Aracena")]
     [TestCase("es", "Vive Cortelazor - Sierra de Aracena")]
     [TestCase("en", "Vive Cortelazor - Sierra de Aracena")]
+    [TestCase("es/privacidad", "Política de privacidad - Vive Cortelazor - Sierra de Aracena")]
+    [TestCase("en/privacy", "Privacy policy - Vive Cortelazor - Sierra de Aracena")]
+    [TestCase("es/cookies", "Política de Cookies - Vive Cortelazor - Sierra de Aracena")]
+    [TestCase("en/cookies", "Cookies policy - Vive Cortelazor - Sierra de Aracena")]
     public async Task HasTitleAsync(string url, string title)
     {
         await Page.GotoAsync(url);
@@ -21,13 +25,32 @@ public class ViveCortelazorTests : PageTest
     [TestCase("", "en", "lang-en")]
     [TestCase("es", "en", "lang-en")]
     [TestCase("en", "es", "lang-es")]
-    public async Task ChangesToLangAsync(string origin, string target, string lang)
+    [TestCase("es/privacidad", "en/privacy", "lang-en")]
+    [TestCase("en/privacy", "es/privacidad", "lang-es")]
+    [TestCase("es/cookies", "en/cookies", "lang-en")]
+    [TestCase("en/cookies", "es/cookies", "lang-es")]
+    public async Task ChangesToLangFromOffcanvasAsync(string origin, string target, string lang)
     {
         await Page.GotoAsync(origin);
 
         await Page.GetByLabel("Toggle navigation").ClickAsync();
         await Page.Locator("id=languages").ClickAsync();
         await Page.Locator($"id={lang}").ClickAsync();
+
+        // Expect a url
+        await Expect(Page).ToHaveURLAsync(new Regex($"{_baseUrl}{target}\\/?$"));
+    }
+
+    [TestCase("es/privacidad", "en/privacy", "lang-en")]
+    [TestCase("en/privacy", "es/privacidad", "lang-es")]
+    [TestCase("es/cookies", "en/cookies", "lang-en")]
+    [TestCase("en/cookies", "es/cookies", "lang-es")]
+    public async Task ChangesToLangFromHorizontalMenuAsync(string origin, string target, string lang)
+    {
+        await Page.GotoAsync(origin);
+
+        await Page.Locator("id=languages-horizontal-menu").ClickAsync();
+        await Page.Locator($"id=languages-horizontal-menu-{lang}").ClickAsync();
 
         // Expect a url
         await Expect(Page).ToHaveURLAsync(new Regex($"{_baseUrl}{target}\\/?$"));
