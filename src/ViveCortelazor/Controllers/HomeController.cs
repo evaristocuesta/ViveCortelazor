4339fc2edff2c55ebe64dd7f259f50c0f39b99fb
evaristocuesta <evaristocg@gmail.com>
2025-02-14 17:22:19 +0100

    Add history page without text #59

--- src/ViveCortelazor/Controllers/HomeController.cs
+++ src/ViveCortelazor/Controllers/HomeController.cs
@@ -17,6 +17,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult History()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
