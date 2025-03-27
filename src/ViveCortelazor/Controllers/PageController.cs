e684b4be3f6f5d7e3083b1949feff21a3dd2b50b
evaristocuesta <evaristocg@gmail.com>
2025-03-27 08:50:50 +0100

    Add blog and page controllers #89

---
+++ src/ViveCortelazor/Controllers/PageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViveCortelazor.Services;
+
+namespace ViveCortelazor.Controllers;
+public class PageController : Controller
+{
+    private readonly IContentService _contentService;
+
+    public PageController(IContentService contentService)
+    {
+        _contentService = contentService;
+    }
+
+    public IActionResult Page(string page)
+    {
+        var viewModel = _contentService.GetContent(
+            "Pages",
+            page,
+            ControllerContext.RouteData.Values["lang"]?.ToString() ?? "es");
+
+        return View(viewModel);
+    }
+}
