67e6901c0c3c5c635e221441975d27d7a2d86d3c
evaristocuesta <evaristocg@gmail.com>
2025-03-30 09:52:43 +0200

    Create constant for base url #89 #95

--- tests/ViveCortelazor.Tests/TestCases/MetaTagsTestCases.cs
+++ tests/ViveCortelazor.Tests/TestCases/MetaTagsTestCases.cs
@@ -5,119 +5,79 @@ namespace ViveCortelazor.Tests.TestCases;
 
 public class MetaTagsTestCases : IEnumerable
 {
+    private const string _baseUrl = "https://www.vivecortelazor.es";
+
     private readonly IContentService _contentService = new ContentService();
 
     public IEnumerator GetEnumerator()
     {
         yield return new TestCaseData(
             "",
-            "https://www.vivecortelazor.es/es",
+            $"{_baseUrl}/es",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en"
-                }
+                { "es", $"{_baseUrl}/es" },
+                { "en", $"{_baseUrl}/en" }
             }
         );
 
         yield return new TestCaseData(
             "es",
-            "https://www.vivecortelazor.es/es",
+            $"{_baseUrl}/es",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en"
-                }
+                { "es", $"{_baseUrl}/es" },
+                { "en", $"{_baseUrl}/en" }
             }
         );
 
         yield return new TestCaseData(
             "en",
-            "https://www.vivecortelazor.es/en",
+            $"{_baseUrl}/en",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en"
-                }
+                { "es", $"{_baseUrl}/es" },
+                { "en", $"{_baseUrl}/en" }
             }
         );
 
         yield return new TestCaseData(
             "es/privacidad",
-            "https://www.vivecortelazor.es/es/privacidad",
+            $"{_baseUrl}/es/privacidad",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es/privacidad"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en/privacy"
-                }
+                { "es", $"{_baseUrl}/es/privacidad" },
+                { "en", $"{_baseUrl}/en/privacy" }
             }
         );
 
         yield return new TestCaseData(
             "en/privacy",
-            "https://www.vivecortelazor.es/en/privacy",
+            $"{_baseUrl}/en/privacy",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es/privacidad"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en/privacy"
-                }
+                { "es", $"{_baseUrl}/es/privacidad" },
+                { "en", $"{_baseUrl}/en/privacy" }
             }
         );
 
         yield return new TestCaseData(
             "es/cookies",
-            "https://www.vivecortelazor.es/es/cookies",
+            $"{_baseUrl}/es/cookies",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es/cookies"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en/cookies"
-                }
+                { "es", $"{_baseUrl}/es/cookies" },
+                { "en", $"{_baseUrl}/en/cookies" }
             }
         );
 
         yield return new TestCaseData(
             "en/cookies",
-            "https://www.vivecortelazor.es/en/cookies",
+            $"{_baseUrl}/en/cookies",
             new Dictionary<string, string>
             {
-                {
-                    "es",
-                    "https://www.vivecortelazor.es/es/cookies"
-                },
-                {
-                    "en",
-                    "https://www.vivecortelazor.es/en/cookies"
-                }
+                { "es", $"{_baseUrl}/es/cookies" },
+                { "en", $"{_baseUrl}/en/cookies" }
             }
         );
 
@@ -133,17 +93,11 @@ public class MetaTagsTestCases : IEnumerable
         {
             yield return new TestCaseData(
                 $"es/blog/{post.Slug}",
-                $"https://www.vivecortelazor.es/es/blog/{post.Slug}",
+                $"{_baseUrl}/es/blog/{post.Slug}",
                 new Dictionary<string, string>
                 {
-                    {
-                        "es",
-                        $"https://www.vivecortelazor.es/es/blog/{post.Slug}"
-                    },
-                    {
-                        "en",
-                        $"https://www.vivecortelazor.es/en/blog/{enPosts[post.Name].Slug}"
-                    }
+                    { "es", $"{_baseUrl}/es/blog/{post.Slug}" },
+                    { "en", $"{_baseUrl}/en/blog/{enPosts[post.Name].Slug}" }
                 }
             );
         }
@@ -152,17 +106,11 @@ public class MetaTagsTestCases : IEnumerable
         {
             yield return new TestCaseData(
                 $"en/blog/{post.Slug}",
-                $"https://www.vivecortelazor.es/en/blog/{post.Slug}",
+                $"{_baseUrl}/en/blog/{post.Slug}",
                 new Dictionary<string, string>
                 {
-                    {
-                        "es",
-                        $"https://www.vivecortelazor.es/es/blog/{esPosts[post.Name].Slug}"
-                    },
-                    {
-                        "en",
-                        $"https://www.vivecortelazor.es/en/blog/{post.Slug}"
-                    }
+                    { "es", $"{_baseUrl}/es/blog/{esPosts[post.Name].Slug}" },
+                    { "en", $"{_baseUrl}/en/blog/{post.Slug}" }
                 }
             );
         }
@@ -179,17 +127,11 @@ public class MetaTagsTestCases : IEnumerable
         {
             yield return new TestCaseData(
                 $"es/{page.Slug}",
-                $"https://www.vivecortelazor.es/es/{page.Slug}",
+                $"{_baseUrl}/es/{page.Slug}",
                 new Dictionary<string, string>
                 {
-                    {
-                        "es",
-                        $"https://www.vivecortelazor.es/es/{page.Slug}"
-                    },
-                    {
-                        "en",
-                        $"https://www.vivecortelazor.es/en/{enPages[page.Name].Slug}"
-                    }
+                    { "es", $"{_baseUrl}/es/{page.Slug}" },
+                    { "en", $"{_baseUrl}/en/{enPages[page.Name].Slug}" }
                 }
             );
         }
@@ -198,17 +140,11 @@ public class MetaTagsTestCases : IEnumerable
         {
             yield return new TestCaseData(
                 $"en/{page.Slug}",
-                $"https://www.vivecortelazor.es/en/{page.Slug}",
+                $"{_baseUrl}/en/{page.Slug}",
                 new Dictionary<string, string>
                 {
-                    {
-                        "es",
-                        $"https://www.vivecortelazor.es/es/{esPages[page.Name].Slug}"
-                    },
-                    {
-                        "en",
-                        $"https://www.vivecortelazor.es/en/{page.Slug}"
-                    }
+                    { "es", $"{_baseUrl}/es/{esPages[page.Name].Slug}" },
+                    { "en", $"{_baseUrl}/en/{page.Slug}" }
                 }
             );
         }
