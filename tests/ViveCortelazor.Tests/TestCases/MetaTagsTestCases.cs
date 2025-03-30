74f59a544fab9fb3cbfd872b4790314a5722061e
evaristocuesta <evaristocg@gmail.com>
2025-03-30 09:39:14 +0200

    Add meta tags tests #89 #95

--- tests/ViveCortelazor.Tests/TestCases/MetaTagsTestCases.cs
+++ tests/ViveCortelazor.Tests/TestCases/MetaTagsTestCases.cs
@@ -1,17 +1,216 @@
 ﻿using System.Collections;
+using ViveCortelazor.Services;
 
 namespace ViveCortelazor.Tests.TestCases;
 
 public class MetaTagsTestCases : IEnumerable
 {
+    private readonly IContentService _contentService = new ContentService();
+
     public IEnumerator GetEnumerator()
     {
-        yield return new object[] { "", "https://www.vivecortelazor.es/es", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es" }, { "en", "https://www.vivecortelazor.es/en" } } };
-        yield return new object[] { "es", "https://www.vivecortelazor.es/es", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es" }, { "en", "https://www.vivecortelazor.es/en" } } };
-        yield return new object[] { "en", "https://www.vivecortelazor.es/en", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es" }, { "en", "https://www.vivecortelazor.es/en" } } };
-        yield return new object[] { "es/privacidad", "https://www.vivecortelazor.es/es/privacidad", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es/privacidad" }, { "en", "https://www.vivecortelazor.es/en/privacy" } } };
-        yield return new object[] { "en/privacy", "https://www.vivecortelazor.es/en/privacy", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es/privacidad" }, { "en", "https://www.vivecortelazor.es/en/privacy" } } };
-        yield return new object[] { "es/cookies", "https://www.vivecortelazor.es/es/cookies", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es/cookies" }, { "en", "https://www.vivecortelazor.es/en/cookies" } } };
-        yield return new object[] { "en/cookies", "https://www.vivecortelazor.es/en/cookies", new Dictionary<string, string> { { "es", "https://www.vivecortelazor.es/es/cookies" }, { "en", "https://www.vivecortelazor.es/en/cookies" } } };
+        yield return new TestCaseData(
+            "",
+            "https://www.vivecortelazor.es/es",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "es",
+            "https://www.vivecortelazor.es/es",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "en",
+            "https://www.vivecortelazor.es/en",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "es/privacidad",
+            "https://www.vivecortelazor.es/es/privacidad",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es/privacidad"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en/privacy"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "en/privacy",
+            "https://www.vivecortelazor.es/en/privacy",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es/privacidad"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en/privacy"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "es/cookies",
+            "https://www.vivecortelazor.es/es/cookies",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es/cookies"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en/cookies"
+                }
+            }
+        );
+
+        yield return new TestCaseData(
+            "en/cookies",
+            "https://www.vivecortelazor.es/en/cookies",
+            new Dictionary<string, string>
+            {
+                {
+                    "es",
+                    "https://www.vivecortelazor.es/es/cookies"
+                },
+                {
+                    "en",
+                    "https://www.vivecortelazor.es/en/cookies"
+                }
+            }
+        );
+
+        var esPosts = _contentService
+            .GetContentList("Content/Blog", "es")
+            .ToDictionary(k => k.Name, k => k);
+
+        var enPosts = _contentService
+            .GetContentList("Content/Blog", "en")
+            .ToDictionary(k => k.Name, k => k);
+
+        foreach (var post in esPosts.Values)
+        {
+            yield return new TestCaseData(
+                $"es/blog/{post.Slug}",
+                $"https://www.vivecortelazor.es/es/blog/{post.Slug}",
+                new Dictionary<string, string>
+                {
+                    {
+                        "es",
+                        $"https://www.vivecortelazor.es/es/blog/{post.Slug}"
+                    },
+                    {
+                        "en",
+                        $"https://www.vivecortelazor.es/en/blog/{enPosts[post.Name].Slug}"
+                    }
+                }
+            );
+        }
+
+        foreach (var post in enPosts.Values)
+        {
+            yield return new TestCaseData(
+                $"en/blog/{post.Slug}",
+                $"https://www.vivecortelazor.es/en/blog/{post.Slug}",
+                new Dictionary<string, string>
+                {
+                    {
+                        "es",
+                        $"https://www.vivecortelazor.es/es/blog/{esPosts[post.Name].Slug}"
+                    },
+                    {
+                        "en",
+                        $"https://www.vivecortelazor.es/en/blog/{post.Slug}"
+                    }
+                }
+            );
+        }
+
+        var esPages = _contentService
+            .GetContentList("Content/Pages", "es")
+            .ToDictionary(k => k.Name, k => k);
+
+        var enPages = _contentService
+            .GetContentList("Content/Pages", "en")
+            .ToDictionary(k => k.Name, k => k);
+
+        foreach (var page in esPages.Values)
+        {
+            yield return new TestCaseData(
+                $"es/{page.Slug}",
+                $"https://www.vivecortelazor.es/es/{page.Slug}",
+                new Dictionary<string, string>
+                {
+                    {
+                        "es",
+                        $"https://www.vivecortelazor.es/es/{page.Slug}"
+                    },
+                    {
+                        "en",
+                        $"https://www.vivecortelazor.es/en/{enPages[page.Name].Slug}"
+                    }
+                }
+            );
+        }
+
+        foreach (var page in enPages.Values)
+        {
+            yield return new TestCaseData(
+                $"en/{page.Slug}",
+                $"https://www.vivecortelazor.es/en/{page.Slug}",
+                new Dictionary<string, string>
+                {
+                    {
+                        "es",
+                        $"https://www.vivecortelazor.es/es/{esPages[page.Name].Slug}"
+                    },
+                    {
+                        "en",
+                        $"https://www.vivecortelazor.es/en/{page.Slug}"
+                    }
+                }
+            );
+        }
     }
 }
