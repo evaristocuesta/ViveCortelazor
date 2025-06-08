5ac147c3ce7c5195f0aadf70365655e82fd3c177
evaristocuesta <evaristocg@gmail.com>
2025-06-08 19:37:30 +0200

    Add where to sleep #87

---
+++ src/ViveCortelazor/Controllers/AccommodationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViveCortelazor.Services;
+
+namespace ViveCortelazor.Controllers;
+
+public class AccommodationController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
