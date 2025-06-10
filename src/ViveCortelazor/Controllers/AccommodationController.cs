dde298aba232429712927ada7761e542816bb045
evaristocuesta <evaristocg@gmail.com>
2025-06-10 20:21:20 +0200

    Update accommodation #87

--- src/ViveCortelazor/Controllers/AccommodationController.cs
+++ src/ViveCortelazor/Controllers/AccommodationController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ViveCortelazor.Services;
+using ViveCortelazor.Models;
 
 namespace ViveCortelazor.Controllers;
 
@@ -7,6 +7,58 @@ public class AccommodationController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        var accommodations = new List<AccommodationViewmodel>
+        {
+            new AccommodationViewmodel
+            {
+                Name = "Casa Piedras",
+                Description = "Casa para 2 personas",
+                Description2 = "1 habitación, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor1.jpg",
+                Url = "https://www.lasierraviva.com/listing/700014676"
+            },
+            new AccommodationViewmodel
+            {
+                Name = "La Casa de mi Tía María",
+                Description = "Casa para 3 personas",
+                Description2 = "1 habitación, 1 cama doble, 1 sofá cama, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor2.jpg",
+                Url = "https://www.lacasademitiamaria.com"
+            },
+            new AccommodationViewmodel
+            {
+                Name = "La Casa de mi Tía María",
+                Description = "Casa para 3 personas",
+                Description2 = "1 habitación, 1 cama doble, 1 sofá cama, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor2.jpg",
+                Url = "https://www.lacasademitiamaria.com"
+            },
+            new AccommodationViewmodel
+            {
+                Name = "La Casa de mi Tía María",
+                Description = "Casa para 3 personas",
+                Description2 = "1 habitación, 1 cama doble, 1 sofá cama, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor2.jpg",
+                Url = "https://www.lacasademitiamaria.com"
+            },
+            new AccommodationViewmodel
+            {
+                Name = "La Casa de mi Tía María",
+                Description = "Casa para 3 personas",
+                Description2 = "1 habitación, 1 cama doble, 1 sofá cama, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor2.jpg",
+                Url = "https://www.lacasademitiamaria.com"
+            },
+            new AccommodationViewmodel
+            {
+                Name = "La Casa de mi Tía María",
+                Description = "Casa para 3 personas",
+                Description2 = "1 habitación, 1 cama doble, 1 sofá cama, 1 baño y cocina",
+                Image = "/images/carousel/cortelazor2.jpg",
+                Url = "https://www.lacasademitiamaria.com"
+            },
+        };
+
+        return View(accommodations);
     }
 }
---
+++ src/ViveCortelazor/Models/AccommodationViewmodel.cs
@@ -0,0 +1,10 @@
+﻿namespace ViveCortelazor.Models;
+
+public class AccommodationViewmodel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Description2 { get; set; }
+    public string Image { get; set; }
+    public string Url { get; set; }
+}
